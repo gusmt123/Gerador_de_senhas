e4a511abb2989a70fa53d15ae9732e4e7c96fe53
gusmt123@gmail.com <gusmt123@gmail.com>
2024-08-26 15:30:35 -0300

    Adiciona função que cria senha que necessáriamente tem letras maiusculas, minusculas e numeros

--- gerador_de_senhas.cpp
+++ gerador_de_senhas.cpp
@@ -3,10 +3,36 @@
 #include <random>
 #include <limits>
 
+    //array constante que tem todos os caracteres especiais, letras maiusculas e minusculas e números
+const char caracteres[] = {
+    ' ', '!', '"', '#', '$', '%', '&', '\'', '(', ')', '*', '+', ',', '-', '.', '/', 
+    ':', ';', '<', '=', '>', '?', '@', 
+    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'
+};
+
+char nums[] = {'0', '1','2','3','4','5','6','7','8','9'};
+
+char alfabeto[] = "abcdefghijklmnopqrstuvwxyz";
+
+char caracteres_Especiais[] = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+
+char letras_Maiusculas[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+
+
+void lida_Com_Respostas_Invalidas();
+
+std::string gera_Senha_Numerica(int& n);
+
+std::string gera_Senha_Letras_Numeros_Talvez_Especiais(int& n);
+
+std::string gera_Senha_Sem_Criterios(int& n);
+
 //Gera senha númerica
-std::string gera_Senha_Numerica(int n)
+std::string gera_Senha_Numerica(int& n)
 {
-    char nums[] = {'0', '1','2','3','4','5','6','7','8','9'};
+    
     std::string senha;
 
     //cria uma instância da classe random_device que serve para usar a situação do hardware para gerar números aleatórios chamda rd
@@ -35,18 +61,51 @@ std::string gera_Senha_Numerica(int n)
 
 }
 
-//função que gera as senhas, ela recebe o parametro n que é o número de caracteres na senha
-std::string gera_Senha_Sem_Criterios(int n)
+std::string gera_Senha_Letras_Numeros_Talvez_Especiais(int& n)
 {
 
-    //array constante que tem todos os caracteres especiais, letras maiusculas e minusculas e números
-    const char caracteres[] = {
-        ' ', '!', '"', '#', '$', '%', '&', '\'', '(', ')', '*', '+', ',', '-', '.', '/', 
-        ':', ';', '<', '=', '>', '?', '@', 
-        'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
-        'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
-        '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'
-    };
+    std::string senha;
+    bool tem_letra_maiuscula = false;
+    bool tem_letra_minuscula = false;
+    bool tem_numero = false;
+
+    //loopa até que a senha tenha letras maiusculas e minusculas, números e caracteres especiais
+    while(!(tem_letra_minuscula && tem_numero && tem_letra_maiuscula))
+    {
+        //gera a senha
+        senha = gera_Senha_Sem_Criterios(n);
+
+        //loopa para cada caracter na senha
+        for (int i = 0; i < n ; i++) 
+        {
+            if (std::isupper(senha[i]))
+            {
+                tem_letra_maiuscula = true;
+            }
+            if (std::islower(senha[i])) 
+            {
+                tem_letra_minuscula = true;
+            }
+            if (std::isdigit(senha[i])) 
+            {
+                tem_numero = true;
+            }
+            
+        }
+
+    }
+
+    
+        //retorna a senha
+        return senha;
+
+}
+
+//função que gera as senhas, ela recebe o parametro n que é o número de caracteres na senha
+std::string gera_Senha_Sem_Criterios(int& n)
+{
+
+
 
     //cria a variável que vai armazenar a senha
     std::string senha;
@@ -89,8 +148,8 @@ void lida_Com_Respostas_Invalidas()
 
 int main()
 {
-    //Controi o menu
-    std::cout << "Digite o tamanho da senha:" << std::endl;
+    //Constroi o menu
+    INICIO_MENU: std::cout << "Digite o tamanho da senha:" << std::endl;
     int tamanho_senha;
     std::cin >> tamanho_senha;
 
@@ -102,29 +161,46 @@ int main()
     }
     else
     {
-        //armazena se a senha tem que ter caracteres especiais
-        std::cout << "Quer uma senha apenas com numeros?" << std::endl;
-        std::cout << "1. Sim" << std::endl;
-        std::cout << "2. Nao" << std::endl;
-        char caracteres_especiais;
-        std::cin >> caracteres_especiais;
+        //armazena o tipo de senha
+        std::cout << "Digite uma opcao:" << std::endl;
+        std::cout << "1. Senha numerica" << std::endl;
+        std::cout << "2. Senha aleatoria" << std::endl;
+        std::cout << "3. Senha com letras e numeros" << std::endl;
+        char tipo_senha;
+        std::cin >> tipo_senha;
 
-        if(std::cin.fail() || caracteres_especiais < '1' && caracteres_especiais > '2')
+        if(std::cin.fail() || tipo_senha < '1' || tipo_senha > '3')
         {
             lida_Com_Respostas_Invalidas();
         }
         else
         {
-            if(caracteres_especiais == '1')
+            if(tipo_senha == '1')
             {
-                std::cout <<gera_Senha_Numerica(tamanho_senha) << std::endl;
+                std::cout << gera_Senha_Numerica(tamanho_senha) << std::endl;
                 
+            }
+            else if(tipo_senha == '2')
+            {
+                std::cout << gera_Senha_Sem_Criterios(tamanho_senha) << std::endl;
+            }
+            else if(tipo_senha == '3')
+            {
+                if(tamanho_senha > 4)
+                {
+                    std::cout << gera_Senha_Letras_Numeros_Talvez_Especiais(tamanho_senha) << std::endl;
+                }
+                else
+                {
+                    std::cout << "Caso queira uma senha com letras e numeros digite peca por uma senha com mais digios" << std::endl;
+                    goto INICIO_MENU;
+                }
             }
             else
             {
-                std::cout <<gera_Senha_Sem_Criterios(tamanho_senha) << std::endl;
+                std::cout << "Digite uma opcao valida:" << std::endl;
+            }
             }
         }
     }
 
-}
